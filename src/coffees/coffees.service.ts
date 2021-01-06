28f7f59fe27a419443b1ebf70e6628fd71f581c9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-01-06 16:38:50 +0700

    Cleanup

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository, InjectConnection } from '@nestjs/typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
 import { Connection, Repository } from 'typeorm';
