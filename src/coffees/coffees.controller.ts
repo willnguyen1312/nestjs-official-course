e43a5cbca51145a9a3b2b18c0240a00d881d77b1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-01-07 14:52:28 +0700

    Add extra API possible response

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
@@ -20,6 +21,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiResponse({ description: 'Forbidden.' })
   @Public()
   @Get('')
   async findAll(
