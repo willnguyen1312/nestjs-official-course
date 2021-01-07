f38e2830e4ab3180bb6e83b35f9a4f6bd224c4fa
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-01-07 13:49:49 +0700

    Add ParseInt Pipe

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -26,7 +27,7 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.coffeesService.findOne(id);
   }
 
